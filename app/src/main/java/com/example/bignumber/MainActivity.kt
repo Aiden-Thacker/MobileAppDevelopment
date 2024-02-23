3eaaa8cc84ea7ad5db4a8637a2c369f7b53bbe75
Aiden-Thacker <aidenthacker86@gmail.com>
2024-02-23 09:54:42 -0600

    Added Words and Definition

--- app/src/main/java/com/example/bignumber/MainActivity.kt
+++ app/src/main/java/com/example/bignumber/MainActivity.kt
@@ -10,6 +10,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.util.Random
 
+data class WordDefinition(var word: String, val definition: String);
+
 class MainActivity : AppCompatActivity() {
 
     private var leftNum :Int = 0;
@@ -17,27 +19,45 @@ class MainActivity : AppCompatActivity() {
     private var score :Int = 0;
     private lateinit var myAdapter : ArrayAdapter<String>;
     private var data_def_list = ArrayList<String>();
-    //var scoreText = findViewById<TextView>(R.id.ShowScoreText);
+    private var wordDefinitions = mutableListOf<WordDefinition>();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        pickRandomNumber();
+        loadDefinition();
         setUpList();
+
 
         val def_list = findViewById<ListView>(R.id.dynamic_def_list);
         def_list.setOnItemClickListener { _, _, index, _ ->
+            var correctDef: String = ""
+            for(wd in wordDefinitions)
+            {
+                if(wd.word == findViewById<TextView>(R.id.defineWord).text.toString())
+                {
+                    correctDef = wd.definition;
+                }
+            }
+
+            if(correctDef == data_def_list[index])
+            {
+                score++;
+            }
+            else
+            {
+                score--;
+            }
+            findViewById<TextView>(R.id.scoreText).text = "Score: $score";
             data_def_list.removeAt(index);
             myAdapter.notifyDataSetChanged();
+            refreshWordAndDefinition();
         };
     }
 
     fun setUpList()
     {
-        data_def_list.add("Three");
-        data_def_list.add("Different");
-        data_def_list.add("Words");
+        refreshWordAndDefinition();
 
         myAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data_def_list);
 
@@ -45,54 +65,48 @@ class MainActivity : AppCompatActivity() {
         def_list.adapter = myAdapter;
     }
 
-    fun leftButtonOnClick(view: View)
+    fun loadDefinition()
     {
-        pickRandomNumber();
-        //increaseScoreCounterIfLeftClicked();
+        wordDefinitions.add(WordDefinition("simple","easy to understand"));
+        wordDefinitions.add(WordDefinition("kotlin","programming language"));
+        wordDefinitions.add(WordDefinition("ui","user interface"));
+        wordDefinitions.add(WordDefinition("ux","user experience"));
     }
 
-    fun rightButtonOnClick(view: View)
+    fun refreshWordAndDefinition()
     {
-        pickRandomNumber();
-        //increaseScoreCounterIfRightClicked();
+        var rand = Random();
+        findViewById<TextView>(R.id.defineWord).text = wordDefinitions[rand.nextInt(wordDefinitions.size)].word;
+
+        data_def_list.clear();
+
+        for(wd in wordDefinitions)
+        {
+            data_def_list.add(wd.definition);
+        }
+
+        data_def_list.shuffle();
     }
 
-    fun pickRandomNumber()
+    /*fun checkWordAndDefinition()
     {
+        var correctDef: String = ""
+        for(wd in wordDefinitions)
+        {
+            if(wd.word == findViewById<TextView>(R.id.defineWord).text.toString())
+            {
+                correctDef = wd.definition;
+            }
+        }
 
-
-        var randNum = Random();
-
-        var leftNum = randNum.nextInt(10);
-        var rightNum = randNum.nextInt(10);
-
-
-    }
-
-    fun increaseScoreCounterIfLeftClicked()
-    {
-
-        if(leftNum >= rightNum)
+        if(correctDef == data_def_list[index])
         {
             score++;
-            //scoreText.setText(score);
-        }else
+        }
+        else
         {
             score--;
-            //scoreText.setText(score);
         }
-    }
-
-    fun increaseScoreCounterIfRightClicked()
-    {
-        if(rightNum >= leftNum)
-        {
-            score++;
-            //scoreText.setText(score);
-        }else
-        {
-            score--;
-            //scoreText.setText(score);
-        }
-    }
+        findViewById<TextView>(R.id.scoreText).text = "Score: $score";
+    }*/
 }
